9d25746c74bc10f5118ef026e6065094268d87cd
Ben Goldhaber <goldhaber.ben@gmail.com>
2018-05-03 11:31:04 -0700

    fill and mult assembler programs

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,75 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// 0 will serve as flag for screen white (0) or screen black (1)
+@0
+M = 0
+(LOOP)
+// If KBD is !0 and M is 0, loop until black
+@KBD
+D = M
+@0
+D = D - M
+@BLACKEN
+D;JGT
+// IF KBD is 0 and M is 0, loop until white
+@WHITEN
+D;JLT
+// Repeat input check indefintely
+@LOOP
+0;JMP
+(BLACKEN)
+// Loop through all coordinates of screen and set pixel = 1
+// i flag for loop
+@1
+M = 0
+@0
+M = 1
+(LOOPSCREENB)
+@1
+D = M
+@8000
+D=D-A
+@LOOP
+D;JGT
+// counter value
+@1
+D = M
+@SCREEN
+//Screen location to whiten/blacken
+A=D+A
+M = -1
+@1
+M = M + 1;
+@LOOPSCREENB
+0;JMP
+// Loop through all coordinates of screen and set pixel = 1
+(WHITEN)
+// Loop through all coordinates of screen and set pixel = 1
+// i flag for loop
+@1
+M = 0
+@0
+M = 0
+(LOOPSCREENW)
+@1
+D = M
+@8000
+D=D-A
+@LOOP
+D;JGT
+// counter value
+@1
+D = M
+@SCREEN
+//Screen location to whiten/blacken
+A=D+A
+M = 0
+@1
+M = M + 1;
+@LOOPSCREENW
+0;JMP
+(END)
+@END
+0;JMP
+
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,29 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+//Gets address of R2 and sets it to 0
+@2
+M=0
+//Symbolic label marker to start the loop
+(LOOP)
+//Subtract one from R1
+@0
+D = M
+D = D-1
+M = D
+@END
+D;JLT //Jumps to end if R0 is < 0
+//Sets D to the value in R2
+@2
+D = M
+//Adds value in R1 to R2
+@1
+D = D + M
+@2
+M = D
+@LOOP
+0;JMP
+(END)
+@END
+0;JMP
+
